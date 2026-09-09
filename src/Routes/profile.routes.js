d61565be8abd2f6f5cff842f333e088ee78e6ff3
ivan Emmanuel Gomez <ivanemmanuelgomez2017@gmail.com>
2026-09-09 12:12:09 -0300

    feat: se agrego una atuentificacion en el profile para que nadie accedad a eso mas que el admin

--- src/Routes/profile.routes.js
+++ src/Routes/profile.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import {getAllPerfiles, getPerfileById, createPerfil, updatePerfil, deletePerfil} from "../Controllers/profile.controller.js";
 import { validationCreateProfile, validationProfileById, validationUpdateProfile } from "../middlewares/validators/profile.validator.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { validate } from "../middlewares/validate.js";
 
 export const PerfilRouter = Router();
 
-PerfilRouter.get("/profiles", getAllPerfiles);
-PerfilRouter.get("/profiles/:id",validationProfileById, validate ,getPerfileById);
-PerfilRouter.post("/profiles",validationCreateProfile,validate, createPerfil);
-PerfilRouter.put("/profiles/:id",validationUpdateProfile,validate, updatePerfil);
-PerfilRouter.delete("/profiles/:id",validationProfileById, validate,  deletePerfil);
+// PerfilRouter.get("/profiles", getAllPerfiles);
+// PerfilRouter.get("/profiles/:id",validationProfileById, validate ,getPerfileById);
+// PerfilRouter.post("/profiles",validationCreateProfile,validate, createPerfil);
+PerfilRouter.put("/profiles/:id",authMiddleware,validationUpdateProfile,validate, updatePerfil);
+// PerfilRouter.delete("/profiles/:id",validationProfileById, validate,  deletePerfil);
